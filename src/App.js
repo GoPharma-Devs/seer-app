14b2e6db52637df76684e984bbf21fe5dbd65f18
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-11-05 01:22:17 -0600

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.svg";
+import "./scss/Index.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="contenedor">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
