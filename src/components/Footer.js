1a482a3b3ae3105cd27f3c1764b9236598683bcd
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-11-25 11:05:38 -0600

    ajuste footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,24 +12,38 @@ function footer() {
       <div className="contenedor-footer">
         <ul>
           <li>
-            <Link to="/">
-              <FaFacebookSquare />
-            </Link>
+            <a href="https://www.instagram.com/seer.latam/">
+              <i>
+                <FaFacebookSquare />
+              </i>
+              seer.latam
+            </a>
           </li>
           <li>
-            <Link to="/">
-              <FaInstagram />
-            </Link>
+            <a href="https://business.facebook.com/latest/home?asset_id=101864688995598&nav_ref=bm_home_redirect">
+              <i>
+                <FaInstagram />
+              </i>
+              SEER LATAM
+            </a>
           </li>
           <li>
-            <Link to="/">
-              <FaLinkedin />
-            </Link>
+            <a href="https://www.linkedin.com/company/seer-latam/?viewAsMember=true">
+              <i>
+                {" "}
+                <FaLinkedin />
+              </i>
+              SEER LATAM
+            </a>
           </li>
           <li>
-            <Link to="/">
-              <FaTwitterSquare />
-            </Link>
+            <a href="https://twitter.com/SEERLATAM">
+              <i>
+                {" "}
+                <FaTwitterSquare />
+              </i>
+              SEERLATAM
+            </a>
           </li>
         </ul>
       </div>
