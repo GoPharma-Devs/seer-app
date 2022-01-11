0f3f68327f3fbdf286d9b37ef17ae6666be82726
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2022-01-10 20:57:12 -0600

    [ADD] Redirects

--- src/views/Registro.js
+++ src/views/Registro.js
@@ -5,7 +5,7 @@ function Registro() {
     <section className="registro" >
 
       <div className="contenedor-form contenedor">
-        <div classname="form">
+        <div className="form">
           {/* Begin Mailchimp Signup Form */}
           <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css" />
           <style type="text/css" dangerouslySetInnerHTML={{ __html: "\n\t#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }\n\t/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.\n\t   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */\n" }} />
@@ -17,40 +17,40 @@ function Registro() {
                 <div className="mc-field-group">
                   <label htmlFor="mce-MMERGE3">Nombre  <span className="asterisk">*</span>
                   </label>
-                  <input type="text" defaultValue name="MMERGE3" className="required" id="mce-MMERGE3" />
+                  <input type="text" defaultValue="nombre" name="MMERGE3" className="required" id="mce-MMERGE3" />
                 </div>
                 <div className="mc-field-group">
                   <label htmlFor="mce-FNAME">Apellido Paterno  <span className="asterisk">*</span>
                   </label>
-                  <input type="text" defaultValue name="FNAME" className="required" id="mce-FNAME" />
+                  <input type="text" defaultValue="nombre" name="FNAME" className="required" id="mce-FNAME" />
                 </div>
                 <div className="mc-field-group">
                   <label htmlFor="mce-LNAME">Apellido Materno  <span className="asterisk">*</span>
                   </label>
-                  <input type="text" defaultValue name="LNAME" className="required" id="mce-LNAME" />
+                  <input type="text" defaultValue="nombre" name="LNAME" className="required" id="mce-LNAME" />
                 </div>
                 <div className="mc-field-group">
                   <label htmlFor="mce-MMERGE6">Lugar de trabajo o de estudio </label>
-                  <input type="text" defaultValue name="MMERGE6" className id="mce-MMERGE6" />
+                  <input type="text" defaultValue="nombre" name="MMERGE6" className id="mce-MMERGE6" />
                 </div>
                 <div className="mc-field-group">
                   <label htmlFor="mce-EMAIL">Email  <span className="asterisk">*</span>
                   </label>
-                  <input type="email" defaultValue name="EMAIL" className="required email" id="mce-EMAIL" />
+                  <input type="email" defaultValue="nombre" name="EMAIL" className="required email" id="mce-EMAIL" />
                 </div>
                 <div className="mc-field-group size1of2">
                   <label htmlFor="mce-PHONE">Teléfono </label>
-                  <input type="text" name="PHONE" className defaultValue id="mce-PHONE" />
+                  <input type="text" name="PHONE" className defaultValue="nombre" id="mce-PHONE" />
                 </div>
                 <div className="mc-field-group">
                   <label htmlFor="mce-MMERGE7">Cédula Profesional </label>
-                  <input type="text" defaultValue name="MMERGE7" className id="mce-MMERGE7" />
+                  <input type="text" defaultValue="Subscribe" name="MMERGE7" className id="mce-MMERGE7" />
                 </div>
                 <div id="mce-responses" className="clear">
                   <div className="response" id="mce-error-response" style={{ display: 'none' }} />
                   <div className="response" id="mce-success-response" style={{ display: 'none' }} />
                 </div>    {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups*/}
-                <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true"><input type="text" name="b_37892f6a3c3f15b6ba401af59_76d3e57aa3" tabIndex={-1} defaultValue /></div>
+                <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true"><input type="text" name="b_37892f6a3c3f15b6ba401af59_76d3e57aa3" tabIndex={-1} /></div>
                 <div className="clear"><input type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></div>
               </div>
             </form>
