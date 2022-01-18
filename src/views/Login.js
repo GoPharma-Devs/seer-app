e062f969d67687e9ca4cb99c15d2922a79623cdb
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2022-01-18 00:03:03 -0600

    ajuste login

--- src/views/Login.js
+++ src/views/Login.js
@@ -44,8 +44,10 @@ const Login = () => {
       setPassword('');
       setSuccess(true);
       setUser(true)
-      if (response?.data === "") {
+      if (response?.status === 200) {
+        console.log("usuario")
         localStorage.setItem("user", user);
+
       }
 
 
@@ -76,7 +78,7 @@ const Login = () => {
             {!user && (
               <>
 
-                <div className="texto"><h1>You are logged in!</h1>
+                <div className="texto"><h1>You are not logged in!</h1>
                   <br />
                   <p>
                     <Link to="/">Ir a home</Link>
@@ -88,9 +90,10 @@ const Login = () => {
               <div className="texto">
 
                 <div className="asistencia">
-                  <iframe id="JotFormIFrame-212609285006048" title="SEER 2022" onload="window.parent.scrollTo(0,0)" allowTransparency="true" allowFullScreen="true" allow="geolocation; microphone; camera" src="https://form.jotform.com/212609285006048" frameBorder={0} style={{ height: 539, border: 'none' }} scrolling="no">
+                  <iframe id="JotFormIFrame-212609285006048" title="SEER 2022" src="https://form.jotform.com/212609285006048" frameBorder={0} style={{ height: 539, border: 'none' }} scrolling="no">
                   </iframe>
                 </div>
+
 
 
               </div>
