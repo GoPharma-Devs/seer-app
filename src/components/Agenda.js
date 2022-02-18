544f589a203f0706e84d0d79751debb2c32a07dd
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2022-02-18 13:41:55 -0600

    ajuste agenda

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,5 +1,5 @@
 
-const AgendaImg = "https://firebasestorage.googleapis.com/v0/b/seer-7d680.appspot.com/o/PROGRAMA_ACADEMICO_17_18_19_FEB_SEER_2022(1).png?alt=media&token=1e31b154-b939-4710-bb8f-e4ef1dc76ce3"
+const AgendaImg = "https://firebasestorage.googleapis.com/v0/b/seer-7d680.appspot.com/o/PROGRAMA_ACADEMICO_17_18_19_FEB_SEER_2022.png?alt=media&token=323d13f2-5eaf-4d21-8246-c5f94b602986"
 export default function Agenda() {
   return (
     <section data-aos="fade-up">
