9aedbe20d9a483070fb7be15ccdc77065cb80e4f
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2022-02-18 09:16:52 -0600

    ajuste

--- src/views/Transmision.js
+++ src/views/Transmision.js
@@ -12,7 +12,7 @@ const Transmision = () => {
 
 
 
-        <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}><iframe src="https://vimeo.com/event/1860866/embed" frameBorder={0} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} /></div>
+        <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}><iframe title="seer día 2" src="https://vimeo.com/event/1860866/embed" frameBorder={0} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} /></div>
 
 
 
