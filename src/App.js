e1bc0493cdc553dbcc84147a03b65d5a9fc01380
Carlos Boyzo <carlos.boor@gmail.com>
2022-04-07 11:10:28 -0500

    update to react 18

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./scss/Index.scss";
 
 import GeneralRoutes from "./routes/GeneralRoutes";
+import { AuthProvider } from '../src/context/AuthProvider'
 
 function App() {
 
   return (
-    <div className="App">
-      <GeneralRoutes />
-    </div>
+    <>
+      <AuthProvider>
+        <div className="App">
+          <GeneralRoutes />
+        </div>
+      </AuthProvider>
+    </>
   );
 }
 
