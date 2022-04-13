8409cb9ab0ec929e5406e0cabc235f94c7e77bba
Carlos Boyzo <carlos.boor@gmail.com>
2022-04-13 00:12:13 -0500

    finish sesion update

--- src/views/DiaUno.js
+++ src/views/DiaUno.js
@@ -44,14 +44,19 @@ function DiaUno() {
 
           <div className='row' style={{ padding: '56.25% 0 0 0', position: 'relative' }}><iframe title='dia-uno' src="https://vimeo.com/showcase/9444749/embed" allowFullScreen frameBorder={0} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} /></div>
 
-          <button className='goback mt-2' onClick={() => navigate(-1)}> <FaArrowAltCircleLeft /> Regresar</button>
+          <div className="row d-flex justify-content-around ">
+            <button className='goback mt-2' onClick={() => navigate(-1)}> <FaArrowAltCircleLeft /> Regresar</button>
+            <button onClick={Logout} className="goback mt-2 ">
+              Cerrar Sesión
+            </button>
+          </div>
         </div>
 
       </section>)}
 
       {!user && (<section className="row App-header  ">
         <div className="container d-flex flex-column align-items-center justify-content-center">
-          <h1>404</h1>
+          <h1>Sesión terminada...</h1>
           <p>regresa al inicio</p>
           <br />
           <button onClick={Logout} className="btn btn-primario"> Ir al incio</button>
@@ -60,9 +65,7 @@ function DiaUno() {
       <div className="row">
         <div className="container d-flex alig-items-center">
 
-          <button style={{ margin: "auto", textAlign: "center" }} onClick={Logout} className="btn-primario cerrar-sesion ">
-            Cerrar Sesión
-          </button>
+
         </div>
       </div>
     </>
