a8d9dc862442c1a40521cbea0952ac2f64d55e07
Carlos Boyzo <carlos.boor@gmail.com>
2022-04-13 12:36:25 -0500

    adjust container

--- src/views/DiaUno.js
+++ src/views/DiaUno.js
@@ -28,10 +28,10 @@ function DiaUno() {
   return (
     <>
       {user && (
-        <section className="section App-header " >
-          <div className="container p-4">
+        <section className="section App-header Diauno" >
+          <div className="container ">
 
-            <div className="mt-2" style={{ padding: '56.25% 0 0 0', position: 'relative' }}><iframe title="SEER 1er Dìa" src="https://vimeo.com/showcase/9444749/embed" allowFullScreen frameBorder={0} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} /></div>
+            <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}><iframe title="SEER 1er Dìa" src="https://vimeo.com/showcase/9444749/embed" allowFullScreen frameBorder={0} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} /></div>
 
 
 
